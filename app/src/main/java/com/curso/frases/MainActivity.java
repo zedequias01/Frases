0f19fa968e10a5f7bffbd9a73bf18bbc4f4cce61
zedequias01 <zsenaxz@gmail.com>
2020-03-04 16:56:16 -0300

    Initial commit

---
+++ app/src/main/java/com/curso/frases/MainActivity.java
@@ -0,0 +1,48 @@
+package com.curso.frases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView primeiraImagem;
+    private String[] opcao = {"as_vezes", "dias_ruins", "imagine", "levanta", "nem_todos", "pra_hoje", "que_o_dia", "sorria", "todo_dia", "um_pequeno_pensamento"};
+    private Button novaImagem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        novaImagem = (Button) findViewById(R.id.botaoProximoId);
+        primeiraImagem = (ImageView) findViewById(R.id.primeiraImagemId);
+        novaImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+
+                Intent intent = new Intent(MainActivity.this, segundaActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
